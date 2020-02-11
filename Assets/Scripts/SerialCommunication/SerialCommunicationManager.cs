0e05b6c829a30a62aa8e2e81024a68b880ad1fb7
Shavedmullet <bsyiem92@gmail.com>
2020-02-11 19:09:35 +1100

    added serial communication test

---
+++ Assets/Scripts/SerialCommunication/SerialCommunicationManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerialCommunicationManager : MonoBehaviour
+{
+    AndroidJavaClass serialCommunicationManagerClass;
+    AndroidJavaObject instance { get { return serialCommunicationManagerClass.GetStatic<AndroidJavaObject>("INSTANCE"); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.serialCommunicationManagerClass = new AndroidJavaClass("com.bsyiem.serialcommunicationplugin.SerialCommunication");
+        this.serialCommunicationManagerClass.CallStatic("instantiate", this.gameObject.name);
+        //this.instance.Call("showText", "is this working?");
+        this.instance.Call("createPhysicaloid");
+        this.instance.Call("openConnection");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SerialCommunication/Test/pluginTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pluginTest : MonoBehaviour
+{
+    void Start()
+    {
+
+        AndroidJavaClass pluginTest = new AndroidJavaClass("com.bsyiem.serialcommunicationplugin.Test");
+        GetComponent<TextMesh>().text = pluginTest.CallStatic<string>("getMessage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
